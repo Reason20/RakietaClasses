2d7c036c901514a7a2fad9216cb4511d995ea304
Slawomir Cieloch <yahatob@gmail.com>
2016-04-13 14:56:27 +0200

    x

---
+++ ConsoleApplication5/ConsoleApplication5/Migrations/201604131256048_Update dbo.Loadsv3.cs
@@ -0,0 +1,16 @@
+namespace ConsoleApplication5.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdatedboLoadsv3 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
