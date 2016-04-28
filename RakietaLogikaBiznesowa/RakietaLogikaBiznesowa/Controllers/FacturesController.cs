b8ff62b1cfa36695285b0b3520338177aba50a59
Kacper <Ryuu4@o2.pl>
2016-04-28 09:51:12 +0200

    Faktury start

---
+++ RakietaLogikaBiznesowa/RakietaLogikaBiznesowa/Controllers/FacturesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RakietaLogikaBiznesowa.Models;
+
+namespace RakietaLogikaBiznesowa.Controllers
+{
+    public class FacturesController : Controller
+    {
+        private Model1 db = new Model1();
+
+        // GET: Factures
+        public async Task<ActionResult> Index()
+        {
+            var facture = db.Facture.Include(f => f.Club).Include(f => f.Contractor).Include(f => f.Editor).Include(f => f.UserCreate).Include(f => f.UserSets2).Include(f => f.UserUpdate);
+            return View(await facture.ToListAsync());
+        }
+
+        // GET: Factures/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Facture facture = await db.Facture.FindAsync(id);
+            if (facture == null)
+            {
+                return HttpNotFound();
+            }
+            return View(facture);
+        }
+
+        // GET: Factures/Create
+        public ActionResult Create()
+        {
+            ViewBag.ClubId = new SelectList(db.Club, "Id", "Name");
+            ViewBag.ContractorId = new SelectList(db.Contractor, "Id", "Name");
+            //ViewBag.LastEditor = new SelectList(db.User, "Id", "FirstName");
+            ViewBag.CreatorId = new SelectList(db.User, "Id", "FirstName");
+            ViewBag.UserId = new SelectList(db.User, "Id", "FirstName");
+            ViewBag.UpdateUserId = new SelectList(db.User, "Id", "FirstName");
+            return View();
+        }
+
+        // POST: Factures/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,OpDate,FactureNumber,NumberSeries,CrDate,Category,ContractorId,CreatorId,UpdateUserId,LastEditTime,LastEditor,InstallmentCount,Value,IsPaid,UserId,ClubId")] Facture facture)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Facture.Add(facture);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ClubId = new SelectList(db.Club, "Id", "Name", facture.ClubId);
+            ViewBag.ContractorId = new SelectList(db.Contractor, "Id", "Name", facture.ContractorId);
+            ViewBag.LastEditor = new SelectList(db.User, "Id", "FirstName", facture.LastEditor);
+            ViewBag.CreatorId = new SelectList(db.User, "Id", "FirstName", facture.CreatorId);
+            ViewBag.UserId = new SelectList(db.User, "Id", "FirstName", facture.UserId);
+            ViewBag.UpdateUserId = new SelectList(db.User, "Id", "FirstName", facture.UpdateUserId);
+            return View(facture);
+        }
+
+        // GET: Factures/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Facture facture = await db.Facture.FindAsync(id);
+            if (facture == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ClubId = new SelectList(db.Club, "Id", "Name", facture.ClubId);
+            ViewBag.ContractorId = new SelectList(db.Contractor, "Id", "Name", facture.ContractorId);
+            ViewBag.LastEditor = new SelectList(db.User, "Id", "FirstName", facture.LastEditor);
+            ViewBag.CreatorId = new SelectList(db.User, "Id", "FirstName", facture.CreatorId);
+            ViewBag.UserId = new SelectList(db.User, "Id", "FirstName", facture.UserId);
+            ViewBag.UpdateUserId = new SelectList(db.User, "Id", "FirstName", facture.UpdateUserId);
+            return View(facture);
+        }
+
+        // POST: Factures/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,OpDate,FactureNumber,NumberSeries,CrDate,Category,ContractorId,CreatorId,UpdateUserId,LastEditTime,LastEditor,InstallmentCount,Value,IsPaid,UserId,ClubId")] Facture facture)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(facture).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ClubId = new SelectList(db.Club, "Id", "Name", facture.ClubId);
+            ViewBag.ContractorId = new SelectList(db.Contractor, "Id", "Name", facture.ContractorId);
+            ViewBag.LastEditor = new SelectList(db.User, "Id", "FirstName", facture.LastEditor);
+            ViewBag.CreatorId = new SelectList(db.User, "Id", "FirstName", facture.CreatorId);
+            ViewBag.UserId = new SelectList(db.User, "Id", "FirstName", facture.UserId);
+            ViewBag.UpdateUserId = new SelectList(db.User, "Id", "FirstName", facture.UpdateUserId);
+            return View(facture);
+        }
+
+        // GET: Factures/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Facture facture = await db.Facture.FindAsync(id);
+            if (facture == null)
+            {
+                return HttpNotFound();
+            }
+            return View(facture);
+        }
+
+        // POST: Factures/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Facture facture = await db.Facture.FindAsync(id);
+            db.Facture.Remove(facture);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
